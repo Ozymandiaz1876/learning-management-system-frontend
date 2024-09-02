20f6fe1543cae65e2929a3112d8d97d0f4c53c73
Chandresh sharma <chandreshsharma06.cs@gmail.com>
2024-09-03 03:57:32 +0530

    [LMS] : feat(registration) : add registration flow

--- src/app/register/index.tsx
+++ src/app/register/index.tsx
@@ -1,28 +1,63 @@
 "use client";
 import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { setAuthData } from "@/app/store/authSlice";
 
 const RegisterPage = () => {
+  const dispatch = useDispatch();
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     email: "",
     password: "",
   });
+  const [error, setError] = useState<string | null>(null);
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Handle registration
+
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/register`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status !== 201) {
+        throw new Error(`Registration failed ${response.data.message}`);
+      }
+
+      const { token, tokenExpires, user } = response.data;
+
+      dispatch(setAuthData({ token, tokenExpires, user }));
+
+      setError(null);
+
+      // TODO add redirection to test start page if user has test data, else redirect to homepage
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.statusText || "Registration failed");
+      } else {
+        setError("An unexpected error occurred");
+      }
+    }
   };
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Register</h1>
+      {error && <p className="text-red-500 mb-4">{error}</p>}
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label className="block text-gray-700">First Name</label>
@@ -42,13 +77,18 @@ const RegisterPage = () => {
         </div>
         <div>
           <label className="block text-gray-700">Email</label>
-          <Input name="email" value={formData.email} onChange={handleChange} />
+          <Input
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label className="block text-gray-700">Password</label>
           <Input
-            type="password"
             name="password"
+            type="password"
             value={formData.password}
             onChange={handleChange}
           />
